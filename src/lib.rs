c48f4eef2110f62a2221aa05bd8781f6dee95710
Fuad Veliev <slowguitarist@anche.no>
2026-08-01 20:58:05 -0400

    better name for consume trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,17 @@
 //! # memsonic
-//! 
+//!
 //! MEMS sensor simulation engine for high-power rocketry.
 
 #![no_std]
 
-use crate::{
-	builder::SimBuilder,
-	model::Model,
-	profile::CannedProfile,
-	sensors::Extract
-};
+use crate::{builder::SimBuilder, model::Model, profile::CannedProfile, sensors::Consume};
 
-mod filters;
-mod model;
-mod sensors;
 mod common;
+mod filters;
 mod math;
+mod model;
 mod profile;
+mod sensors;
 
 pub mod builder;
 
@@ -47,15 +42,15 @@ macro_rules! getter (($n:literal, $f:ident, $d:ident, $t:ty) => (
 	Returns a result that always contains the most recent ", $n, "
 	 reading from the simulation. `Ok` indicates that the reading
 	is new and `Err` -- that it did not change since last call.
-	
+
 	A call to this function lazily evaluates the engine's state
 	since the last call to any of the [`Simulation`] methods.
-	
+
 	## Examples
-	
+
 	Assume a builder `sim` previously created and populated
 	with kinematic targets, and current time `now`, in ms.
-	
+
 	```
 	if let Ok(reading) = sim.", stringify!($f), "(now) {
 		// consume reading
@@ -63,7 +58,7 @@ macro_rules! getter (($n:literal, $f:ident, $d:ident, $t:ty) => (
 	```
 	")]
 	pub fn $f(&mut self, tim: u32) -> Result<$t, $t> {
-		self.step(tim).m.$d.extract()
+		self.step(tim).m.$d.consume()
 	}
 ));
 
@@ -72,109 +67,109 @@ macro_rules! getter (($n:literal, $f:ident, $d:ident, $t:ty) => (
 /////////////////////////////////////////////////////////////////////////////
 
 pub struct Simulation<const N: usize> {
-	m: Model,
-	p: CannedProfile<N>,
-	tim: u32,
-	delay: u32,
-	rate: u32
+    m: Model,
+    p: CannedProfile<N>,
+    tim: u32,
+    delay: u32,
+    rate: u32,
 }
 
 impl<const N: usize> Simulation<N> {
-	/// Creates a new simulation, consuming the builder `b`.
-	/// 
-	/// `new()` will return the non-copyable simulation struct itself.
-	/// 
-	/// `delay` specifies the time during which no work should be done
-	/// towards kinematic targets. This is useful to simulate inherent
-	/// sensor drift while a vehicle is stationary.
-	pub fn new(mut b: impl SimBuilder, delay: u32) -> Self {
-		let rate = b.rate();
-		Self {
-			m: Model::new(rate, b.imu(), b.baro()),
-			p: CannedProfile::new(delay),
-			tim: 0,
-			delay,
-			rate
-		}
-	}
+    /// Creates a new simulation, consuming the builder `b`.
+    ///
+    /// `new()` will return the non-copyable simulation struct itself.
+    ///
+    /// `delay` specifies the time during which no work should be done
+    /// towards kinematic targets. This is useful to simulate inherent
+    /// sensor drift while a vehicle is stationary.
+    pub fn new(mut b: impl SimBuilder, delay: u32) -> Self {
+        let rate = b.rate();
+        Self {
+            m: Model::new(rate, b.imu(), b.baro()),
+            p: CannedProfile::new(delay),
+            tim: 0,
+            delay,
+            rate,
+        }
+    }
 
-	/// Adds an absolute kinematic target at relative time offset.
-	/// 
-	/// `fix()` will return a unique reference to the simulation struct
-	/// for the further data point chaining.
-	/// 
-	/// ## Examples
-	/// 
-	/// Assume a builder `b` previously created.
-	/// 
-	/// ```
-	/// let sim = Simulation::new(b, 1000);
-	/// sim
-	///     .fix(([0.2, 0.3, 12.2], [0.1, 0.4, 2.3]), 200)
-	///     .fix(([0.3, 0.2, 26.1], [0.3, 0.3, 1.0]), 150);
-	/// ```
-	/// 
-	/// The first call to [`fix`] will create a new data point with the
-	/// timestamp 1200 ms from the creation of `sim`. The first triplet
-	/// will represent accelerometer values and the second -- gyroscope.
-	/// 
-	/// The second call to [`fix`] will create a new data point with the
-	/// timestamp 1350 ms, using new data directly as new target.
-	pub fn fix(&mut self, f: Motion, dur: u32) -> &mut Self {
-		self.p.append(f, dur, false);
-		self
-	}
+    /// Adds an absolute kinematic target at relative time offset.
+    ///
+    /// `fix()` will return a unique reference to the simulation struct
+    /// for the further data point chaining.
+    ///
+    /// ## Examples
+    ///
+    /// Assume a builder `b` previously created.
+    ///
+    /// ```
+    /// let sim = Simulation::new(b, 1000);
+    /// sim
+    ///     .fix(([0.2, 0.3, 12.2], [0.1, 0.4, 2.3]), 200)
+    ///     .fix(([0.3, 0.2, 26.1], [0.3, 0.3, 1.0]), 150);
+    /// ```
+    ///
+    /// The first call to [`fix`] will create a new data point with the
+    /// timestamp 1200 ms from the creation of `sim`. The first triplet
+    /// will represent accelerometer values and the second -- gyroscope.
+    ///
+    /// The second call to [`fix`] will create a new data point with the
+    /// timestamp 1350 ms, using new data directly as new target.
+    pub fn fix(&mut self, f: Motion, dur: u32) -> &mut Self {
+        self.p.append(f, dur, false);
+        self
+    }
 
-	/// Adds a relative kinematic target at relative time offset.
-	/// 
-	/// `add()` will return a unique reference to the simulation struct
-	/// for the further data point chaining.
-	/// 
-	/// ## Examples
-	/// 
-	/// Assume a builder `b` previously created.
-	/// 
-	/// ```
-	/// let sim = Simulation::new(b, 1000);
-	/// sim
-	///     .fix(([0.2, 0.3, 12.2], [0.1, 0.4, 2.3]), 200)
-	///     .add(([0.3, 0.2, 26.1], [0.3, 0.3, 1.0]), 150);
-	/// ```
-	/// 
-	/// The call to [`add`] will create a new data point with the timestamp
-	/// 1350 ms from the creation of `sim`, and the provided kinematic data
-	/// will be added to that of a previous data point. The new target, in
-	/// this case, will hold `[0.5, 0.5, 38.3]` and `[0.4, 0.7, 3.3]`. 
-	pub fn add(&mut self, f: Motion, dur: u32) -> &mut Self {
-		self.p.append(f, dur, true);
-		self
-	}
+    /// Adds a relative kinematic target at relative time offset.
+    ///
+    /// `add()` will return a unique reference to the simulation struct
+    /// for the further data point chaining.
+    ///
+    /// ## Examples
+    ///
+    /// Assume a builder `b` previously created.
+    ///
+    /// ```
+    /// let sim = Simulation::new(b, 1000);
+    /// sim
+    ///     .fix(([0.2, 0.3, 12.2], [0.1, 0.4, 2.3]), 200)
+    ///     .add(([0.3, 0.2, 26.1], [0.3, 0.3, 1.0]), 150);
+    /// ```
+    ///
+    /// The call to [`add`] will create a new data point with the timestamp
+    /// 1350 ms from the creation of `sim`, and the provided kinematic data
+    /// will be added to that of a previous data point. The new target, in
+    /// this case, will hold `[0.5, 0.5, 38.3]` and `[0.4, 0.7, 3.3]`.
+    pub fn add(&mut self, f: Motion, dur: u32) -> &mut Self {
+        self.p.append(f, dur, true);
+        self
+    }
 
-	/// Breaks elapsed time into equal intervals equal to engine rate
-	/// and calls derivation logic multiple times. Lazy.
-	fn step(&mut self, mut tim: u32) -> &mut Self {
-		if tim > self.delay {
-			tim -= self.delay;
-		}
-		
-		if tim.wrapping_sub(self.tim) < self.rate {
-			return self
-		}
+    /// Breaks elapsed time into equal intervals equal to engine rate
+    /// and calls derivation logic multiple times. Lazy.
+    fn step(&mut self, mut tim: u32) -> &mut Self {
+        if tim > self.delay {
+            tim -= self.delay;
+        }
 
-		if let Some(target) = self.p.linearize(tim) {
-			let secs = self.rate as f32 / 1000.0;
+        if tim.wrapping_sub(self.tim) < self.rate {
+            return self;
+        }
 
-			while tim.wrapping_sub(self.tim) >= self.rate {
-				self.tim = self.tim.wrapping_add(self.rate);
-				self.m.derive(secs, target);
-			}
-		}
+        if let Some(target) = self.p.linearize(tim) {
+            let secs = self.rate as f32 / 1000.0;
 
-		self
-	}
+            while tim.wrapping_sub(self.tim) >= self.rate {
+                self.tim = self.tim.wrapping_add(self.rate);
+                self.m.derive(secs, target);
+            }
+        }
 
-	getter!("accelerometer", accel, acl, XYZ);
-	getter!("gyroscope", angvel, gyr, XYZ);
-	getter!("magnetometer", magfield, mag, XYZ);
-	getter!("barometer", pressure, bar, f32);
+        self
+    }
+
+    getter!("accelerometer", accel, acl, XYZ);
+    getter!("gyroscope", angvel, gyr, XYZ);
+    getter!("magnetometer", magfield, mag, XYZ);
+    getter!("barometer", pressure, bar, f32);
 }
--- src/sensors.rs
+++ src/sensors.rs
@@ -1,27 +1,27 @@
-use libm::{expf};
+use libm::expf;
 
 use crate::Alignment;
 use crate::Bias;
 use crate::XYZ;
 use crate::common::*;
 use crate::filters::BiquadType::LowPass;
+use crate::filters::*;
 use crate::math::*;
 use crate::model::*;
-use crate::filters::*;
 
 #[derive(Clone, Copy)]
 pub(crate) struct Collector {
-	pub(crate) odr: u32,
-	pub(crate) cutoff: f32,
-	pub(crate) qbw: f32,
-	pub(crate) sens: f32
+    pub(crate) odr: u32,
+    pub(crate) cutoff: f32,
+    pub(crate) qbw: f32,
+    pub(crate) sens: f32,
 }
 
 #[derive(Clone, Copy)]
 pub(crate) struct Disperser<const N: usize> {
-	pub(crate) sigma: f32,
-	pub(crate) bias: Bias<N>,
-	pub(crate) align: Alignment<N>,
+    pub(crate) sigma: f32,
+    pub(crate) bias: Bias<N>,
+    pub(crate) align: Alignment<N>,
 }
 
 pub(crate) trait NoiseMode {}
@@ -33,296 +33,257 @@ pub(crate) struct Fast;
 impl NoiseMode for Fast {}
 
 pub(crate) trait Distort<T: Copy, M: NoiseMode> {
-	fn distort(&self) -> T;
+    fn distort(&self) -> T;
 }
 
 pub(crate) struct Gaussian(f32);
 
 impl Distort<f32, Normal> for Gaussian {
-	fn distort(&self) -> f32 {
-		let mut s = 0.0;
+    fn distort(&self) -> f32 {
+        let mut s = 0.0;
 
-		for _ in 0..12 {
-			s += (rand() as f32) / (u32::MAX as f32);
-		}
+        for _ in 0..12 {
+            s += (rand() as f32) / (u32::MAX as f32);
+        }
 
-		(s - 6.0) * self.0
-	}
+        (s - 6.0) * self.0
+    }
 }
 
 impl Distort<f32, Fast> for Gaussian {
-	fn distort(&self) -> f32 {
-		let mut s = 0;
+    fn distort(&self) -> f32 {
+        let mut s = 0;
 
-		for _ in 0..3 {
-			let r = rand();
-			s += (r & 0xFF) + ((r >> 8) & 0xFF) + ((r >> 16) & 0xFF) + (r >> 24);
-		}
+        for _ in 0..3 {
+            let r = rand();
+            s += (r & 0xFF) + ((r >> 8) & 0xFF) + ((r >> 16) & 0xFF) + (r >> 24);
+        }
 
-		(s as f32 - 1530.0) / 256.0 * self.0
-	}
+        (s as f32 - 1530.0) / 256.0 * self.0
+    }
 }
 
 pub(crate) trait Evaluate {
-	fn evaluate(&mut self, s: &ModelState) -> &Self;
+    fn evaluate(&mut self, s: &ModelState) -> &Self;
 }
 
 struct SensorCore<const N: usize> {
-	cic: CICConf,
-	biq: BiquadCoef,
-	fir: [EmuCIC; N],
-	iir: [Biquad; N],
-	meas: [f32; N],
-	rel: bool
+    cic: CICConf,
+    biq: BiquadCoef,
+    fir: [EmuCIC; N],
+    iir: [Biquad; N],
+    meas: [f32; N],
+    rel: bool,
 }
 
 impl<const N: usize> SensorCore<N> {
-	pub(crate) fn new(
-		odr: u32,
-		engrate: u32,
-		biqtype: BiquadType,
-		sens: f32
-	) -> Self {
-		Self {
-			cic: CICConf::new(odr / engrate, sens),
-			biq: BiquadCoef::derive(biqtype, odr as f32),
-			fir: [EmuCIC::new(); N],
-			iir: [Biquad::new(); N],
-			meas: [0.0; N],
-			rel: false
-		}
-	}
+    pub(crate) fn new(odr: u32, engrate: u32, biqtype: BiquadType, sens: f32) -> Self {
+        Self {
+            cic: CICConf::new(odr / engrate, sens),
+            biq: BiquadCoef::derive(biqtype, odr as f32),
+            fir: [EmuCIC::new(); N],
+            iir: [Biquad::new(); N],
+            meas: [0.0; N],
+            rel: false,
+        }
+    }
 }
 
 impl<const N: usize> SensorCore<N> {
-	fn collect(&mut self, mut n: impl Iterator<Item = f32>) {
-		for i in 0..N {
-			let samp = n.next().expect("Sensor axes must be equal");
+    fn collect(&mut self, mut n: impl Iterator<Item = f32>) {
+        for i in 0..N {
+            let samp = n.next().expect("Sensor axes must be equal");
 
-			if let Some(dec) = self.fir[i].filter(&self.cic, samp) {
-				self.rel = true;
-				self.meas[i] = self.iir[i].filter(&self.biq, dec);
-			}
-		}
-	}
-} 
+            if let Some(dec) = self.fir[i].filter(&self.cic, samp) {
+                self.rel = true;
+                self.meas[i] = self.iir[i].filter(&self.biq, dec);
+            }
+        }
+    }
+}
 
 struct BiasedAxis<const N: usize> {
-	align: Alignment<N>,
-	bias: [f32; N],
-	ng: Gaussian,
+    align: Alignment<N>,
+    bias: [f32; N],
+    ng: Gaussian,
 }
 
 impl<const N: usize> BiasedAxis<N> {
-	pub(crate) fn new(
-		sigma: f32,
-		bias: [f32; N],
-		align: Alignment<N>,
-	) -> Self {
-		Self { align, bias, ng: Gaussian(sigma) }
-	}
+    pub(crate) fn new(sigma: f32, bias: [f32; N], align: Alignment<N>) -> Self {
+        Self {
+            align,
+            bias,
+            ng: Gaussian(sigma),
+        }
+    }
 }
 
 impl<const N: usize> BiasedAxis<N> {
-	fn disperse(&mut self, phys: &[f32; N]) -> impl Iterator<Item = f32> {
-		self.align.iter()
-			.zip(self.bias.iter().copied())
-			.map(|(axis, bias)| {
-				let mut raw = bias;
+    fn disperse(&mut self, phys: &[f32; N]) -> impl Iterator<Item = f32> {
+        self.align
+            .iter()
+            .zip(self.bias.iter().copied())
+            .map(|(axis, bias)| {
+                let mut raw = bias;
 
-				for i in 0..N {
-					raw += axis[i] * phys[i];
-				}
-				
-				raw + <Gaussian as Distort<f32, Normal>>::distort(&self.ng)
-			})
-	}
+                for i in 0..N {
+                    raw += axis[i] * phys[i];
+                }
+
+                raw + <Gaussian as Distort<f32, Normal>>::distort(&self.ng)
+            })
+    }
 }
 
 pub(crate) struct Accelerometer {
-	k: SensorCore<3>,
-	m: BiasedAxis<3>,
-	vibsens: f32
+    k: SensorCore<3>,
+    m: BiasedAxis<3>,
+    vibsens: f32,
 }
 
 impl Accelerometer {
-	pub(crate) fn new(
-		engrate: u32,
-		vibsens: f32,
-		k: Collector,
-		m: Disperser<3>
-	) -> Self {
-		Self {
-			k: SensorCore::new(
-				k.odr,
-				engrate,
-				LowPass(k.cutoff, k.qbw),
-				k.sens
-			),
-			m: BiasedAxis::new(m.sigma, m.bias, m.align),
-			vibsens
-		}
-	}
+    pub(crate) fn new(engrate: u32, vibsens: f32, k: Collector, m: Disperser<3>) -> Self {
+        Self {
+            k: SensorCore::new(k.odr, engrate, LowPass(k.cutoff, k.qbw), k.sens),
+            m: BiasedAxis::new(m.sigma, m.bias, m.align),
+            vibsens,
+        }
+    }
 }
 
 impl Evaluate for Accelerometer {
-	fn evaluate(&mut self, s: &ModelState) -> &Self {
-		self.m.ng.0 = s.vib * self.vibsens;
-		self.k.collect(self.m.disperse(&s.acc));
-		self
-	}
+    fn evaluate(&mut self, s: &ModelState) -> &Self {
+        self.m.ng.0 = s.vib * self.vibsens;
+        self.k.collect(self.m.disperse(&s.acc));
+        self
+    }
 }
 
 pub(crate) struct Gyroscope {
-	k: SensorCore<3>,
-	m: BiasedAxis<3>,
-	gsens: f32
+    k: SensorCore<3>,
+    m: BiasedAxis<3>,
+    gsens: f32,
 }
 
 impl Gyroscope {
-	pub(crate) fn new(
-		engrate: u32,
-		gsens: f32,
-		k: Collector,
-		m: Disperser<3>
-	) -> Self {
-		Self {
-			k: SensorCore::new(
-				k.odr,
-				engrate,
-				LowPass(k.cutoff, k.qbw),
-				k.sens
-			),
-			m: BiasedAxis::new(m.sigma, m.bias, m.align),
-			gsens
-		}
-	}
+    pub(crate) fn new(engrate: u32, gsens: f32, k: Collector, m: Disperser<3>) -> Self {
+        Self {
+            k: SensorCore::new(k.odr, engrate, LowPass(k.cutoff, k.qbw), k.sens),
+            m: BiasedAxis::new(m.sigma, m.bias, m.align),
+            gsens,
+        }
+    }
 }
 
 impl Evaluate for Gyroscope {
-	fn evaluate(&mut self, s: &ModelState) -> &Self {
-		self.k.collect(self.m.disperse(&s.ang)
-			.zip(s.acc.iter().copied())
-			.map(|(raw, acc)| {
-				raw + acc * self.gsens
-			})
-		);
-		self
-	}
+    fn evaluate(&mut self, s: &ModelState) -> &Self {
+        self.k.collect(
+            self.m
+                .disperse(&s.ang)
+                .zip(s.acc.iter().copied())
+                .map(|(raw, acc)| raw + acc * self.gsens),
+        );
+        self
+    }
 }
 
 pub(crate) struct Magnetometer {
-	k: SensorCore<3>,
-	m: BiasedAxis<3>
+    k: SensorCore<3>,
+    m: BiasedAxis<3>,
 }
 
 impl Magnetometer {
-	pub(crate) fn new(
-		engrate: u32,
-		k: Collector,
-		m: Disperser<3>
-	) -> Self {
-		Self {
-			k: SensorCore::new(
-				k.odr,
-				engrate,
-				LowPass(k.cutoff, k.qbw),
-				k.sens
-			),
-			m: BiasedAxis::new(m.sigma, m.bias, m.align),
-		}
-	}
+    pub(crate) fn new(engrate: u32, k: Collector, m: Disperser<3>) -> Self {
+        Self {
+            k: SensorCore::new(k.odr, engrate, LowPass(k.cutoff, k.qbw), k.sens),
+            m: BiasedAxis::new(m.sigma, m.bias, m.align),
+        }
+    }
 }
 
 impl Evaluate for Magnetometer {
-	fn evaluate(&mut self, s: &ModelState) -> &Self {
-		let bodyf = s.q.rotate_w2b(EARTH_MAGFIELD);
-		self.k.collect(self.m.disperse(&bodyf));
-		self
-	}
+    fn evaluate(&mut self, s: &ModelState) -> &Self {
+        let bodyf = s.q.rotate_w2b(EARTH_MAGFIELD);
+        self.k.collect(self.m.disperse(&bodyf));
+        self
+    }
 }
 
 pub(crate) struct Barometer {
-	k: SensorCore<1>,
-	ng: Gaussian,
-	bias: f32,
-	lag: f32,
-	tmp: f32
+    k: SensorCore<1>,
+    ng: Gaussian,
+    bias: f32,
+    lag: f32,
+    tmp: f32,
 }
 
 impl Barometer {
-	pub(crate) fn new(
-		engrate: u32,
-		k: Collector,
-		m: Disperser<1>,
-	) -> Self {
-		Self {
-			k: SensorCore::new(
-				k.odr,
-				engrate,
-				LowPass(k.cutoff, k.qbw),
-				k.sens
-			),
-			ng: Gaussian(m.sigma),
-			bias: m.bias[0],
-			lag: m.align[0][0],
-			tmp: SEA_TMP
-		}
-	}
+    pub(crate) fn new(engrate: u32, k: Collector, m: Disperser<1>) -> Self {
+        Self {
+            k: SensorCore::new(k.odr, engrate, LowPass(k.cutoff, k.qbw), k.sens),
+            ng: Gaussian(m.sigma),
+            bias: m.bias[0],
+            lag: m.align[0][0],
+            tmp: SEA_TMP,
+        }
+    }
 }
 
 impl Evaluate for Barometer {
-	fn evaluate(&mut self, s: &ModelState) -> &Self {
-		let mach = s.vel.norm() / sqrt(s.tmp * SP_GAS * SP_HEAT);
+    fn evaluate(&mut self, s: &ModelState) -> &Self {
+        let mach = s.vel.norm() / sqrt(s.tmp * SP_GAS * SP_HEAT);
 
-		let shock = if mach > 0.8 && mach < 1.2 {
-			SHOCK_DEPTH * expf(-100.0 * sq(mach - 1.0))
-		} else {
-			0.0
-		};
+        let shock = if mach > 0.8 && mach < 1.2 {
+            SHOCK_DEPTH * expf(-100.0 * sq(mach - 1.0))
+        } else {
+            0.0
+        };
 
-		self.tmp += (s.tmp - self.tmp) * self.lag;
-		let drift = (self.tmp - 20.0) * DRIFT_PER_C;
+        self.tmp += (s.tmp - self.tmp) * self.lag;
+        let drift = (self.tmp - 20.0) * DRIFT_PER_C;
 
-		let prs = [ s.prs + shock + drift + self.bias +
-			<Gaussian as Distort<f32, Normal>>::distort(&self.ng) ];
+        let prs = [s.prs
+            + shock
+            + drift
+            + self.bias
+            + <Gaussian as Distort<f32, Normal>>::distort(&self.ng)];
 
-		self.k.collect(prs.iter().copied());
-		self
-	}
+        self.k.collect(prs.iter().copied());
+        self
+    }
 }
 
 // FIXME replace crutch with something better
-pub(crate) trait Extract<T> {
-	fn extract(&mut self) -> Result<T, T>;
+pub(crate) trait Consume<T> {
+    fn consume(&mut self) -> Result<T, T>;
 }
 
 macro_rules! extract3axis {
-	($sensor:ident) => {
-		impl Extract<XYZ> for $sensor {
-			fn extract(&mut self) -> Result<XYZ, XYZ> {
-				if self.k.rel {
-					self.k.rel = false;
-					Ok(self.k.meas)
-				} else {
-					Err(self.k.meas)
-				}
-			}
-		}
-	};
+    ($sensor:ident) => {
+        impl Consume<XYZ> for $sensor {
+            fn consume(&mut self) -> Result<XYZ, XYZ> {
+                if self.k.rel {
+                    self.k.rel = false;
+                    Ok(self.k.meas)
+                } else {
+                    Err(self.k.meas)
+                }
+            }
+        }
+    };
 }
 
 extract3axis!(Accelerometer);
 extract3axis!(Gyroscope);
 extract3axis!(Magnetometer);
 
-impl Extract<f32> for Barometer {
-	fn extract(&mut self) -> Result<f32, f32> {
-		if self.k.rel {
-			self.k.rel = false;
-			Ok(self.k.meas[0])
-		} else {
-			Err(self.k.meas[0])
-		}
-	}
+impl Consume<f32> for Barometer {
+    fn consume(&mut self) -> Result<f32, f32> {
+        if self.k.rel {
+            self.k.rel = false;
+            Ok(self.k.meas[0])
+        } else {
+            Err(self.k.meas[0])
+        }
+    }
 }
